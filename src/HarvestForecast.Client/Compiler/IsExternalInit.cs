18badfa95f30dd03bc6edda18bd324b635c4f8d6
Brad Glass <brad.glass@razor.co.uk>
2022-04-12 15:12:27 +0100

    Add object model for the WhoAmI endpoint

---
+++ src/HarvestForecast.Client/Compiler/IsExternalInit.cs
@@ -0,0 +1,5 @@
+﻿// ReSharper disable once CheckNamespace Required for C# 9 Record Classes
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
---
+++ src/HarvestForecast.Client/CurrentUser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace HarvestForecast.Client;
+
+/// <summary>
+///     Contains information about the current Forecast user.
+/// </summary>
+/// <param name="Id">The ID of the user.</param>
+/// <param name="AccountIds">The IDs of the accounts that this user is part of.</param>
+public record CurrentUser( [ property : JsonPropertyName( "id" ) ] int Id,
+                           [ property : JsonPropertyName( "account_ids" ) ] IReadOnlyCollection<int> AccountIds );
+
+internal record CurrentUserContainer( [ property : JsonPropertyName( "current_user" ) ] CurrentUser CurrentUser );
