1657effbf2e3784277f5e56e82fee08c372be545
Brad Glass <brad.glass@razor.co.uk>
2022-04-14 14:33:43 +0100

    Add a WorkingDays class to capture day of week information in a performant way

---
+++ src/HarvestForecast.Client/Entities/WorkingDays.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HarvestForecast.Client.Entities;
+
+/// <summary>
+///     Indicates the days of the week that a <see cref="Person" /> works.
+/// </summary>
+public readonly struct WorkingDays
+{
+    [ Flags ]
+    private enum FlaggedDays
+    {
+        None = 0,
+        Sunday = 1 << 0,
+        Monday = 1 << 1,
+        Tuesday = 1 << 2,
+        Wednesday = 1 << 3,
+        Thursday = 1 << 4,
+        Friday = 1 << 5,
+        Saturday = 1 << 6
+    }
+
+    private readonly FlaggedDays flags;
+
+    private WorkingDays( FlaggedDays flags )
+    {
+        this.flags = flags;
+    }
+
+    /// <summary>
+    ///     Creates a new <see cref="WorkingDays" /> from a collection of <see cref="DayOfWeek" />s.
+    /// </summary>
+    public WorkingDays( IEnumerable<DayOfWeek> days ) : this( ConvertToFlags( days ) ) { }
+
+    private static FlaggedDays ConvertToFlags( IEnumerable<DayOfWeek> days )
+    {
+        var flags = FlaggedDays.None;
+
+        foreach ( var day in days )
+        {
+            var flag = MapToFlag( day );
+            flags |= flag;
+        }
+
+        return flags;
+    }
+
+    private static FlaggedDays MapToFlag( DayOfWeek day )
+    {
+        return day switch
+        {
+            DayOfWeek.Friday => FlaggedDays.Friday,
+            DayOfWeek.Monday => FlaggedDays.Monday,
+            DayOfWeek.Saturday => FlaggedDays.Saturday,
+            DayOfWeek.Sunday => FlaggedDays.Sunday,
+            DayOfWeek.Thursday => FlaggedDays.Thursday,
+            DayOfWeek.Tuesday => FlaggedDays.Tuesday,
+            DayOfWeek.Wednesday => FlaggedDays.Wednesday,
+            _ => throw new ArgumentOutOfRangeException( nameof( day ), day, null )
+        };
+    }
+
+    /// <summary>
+    ///     Checks if Monday is active.
+    /// </summary>
+    public bool Monday => flags.HasFlag( FlaggedDays.Monday );
+
+    /// <summary>
+    ///     Checks if Tuesday is active.
+    /// </summary>
+    public bool Tuesday => flags.HasFlag( FlaggedDays.Tuesday );
+
+    /// <summary>
+    ///     Checks if Wednesday is active.
+    /// </summary>
+    public bool Wednesday => flags.HasFlag( FlaggedDays.Wednesday );
+
+    /// <summary>
+    ///     Checks if Thursday is active.
+    /// </summary>
+    public bool Thursday => flags.HasFlag( FlaggedDays.Thursday );
+
+    /// <summary>
+    ///     Checks if Friday is active.
+    /// </summary>
+    public bool Friday => flags.HasFlag( FlaggedDays.Friday );
+
+    /// <summary>
+    ///     Checks if Saturday is active.
+    /// </summary>
+    public bool Saturday => flags.HasFlag( FlaggedDays.Saturday );
+
+    /// <summary>
+    ///     Checks if Sunday is active.
+    /// </summary>
+    public bool Sunday => flags.HasFlag( FlaggedDays.Sunday );
+
+    /// <summary>
+    ///     Enumerates the days that are active.
+    /// </summary>
+    public IEnumerable<DayOfWeek> GetDays()
+    {
+        if ( Monday ) yield return DayOfWeek.Monday;
+        if ( Tuesday ) yield return DayOfWeek.Tuesday;
+        if ( Wednesday ) yield return DayOfWeek.Wednesday;
+        if ( Thursday ) yield return DayOfWeek.Thursday;
+        if ( Friday ) yield return DayOfWeek.Friday;
+        if ( Saturday ) yield return DayOfWeek.Saturday;
+        if ( Sunday ) yield return DayOfWeek.Sunday;
+    }
+
+    /// <summary>
+    ///     Makes a <paramref name="day" /> active and returns a new <see cref="WorkingDays" />.
+    /// </summary>
+    public static WorkingDays operator +( WorkingDays days, DayOfWeek day )
+    {
+        var flag = MapToFlag( day );
+        var flags = days.flags | flag;
+
+        return new WorkingDays( flags );
+    }
+
+    /// <summary>
+    ///     Makes a <paramref name="day" /> inactive and returns a new <see cref="WorkingDays" />.
+    /// </summary>
+    public static WorkingDays operator -( WorkingDays days, DayOfWeek day )
+    {
+        var flag = MapToFlag( day );
+        var flags = days.flags & ~ flag;
+
+        return new WorkingDays( flags );
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return string.Join( " | ", GetDays().Select( d => d.ToString() ) );
+    }
+}
---
+++ test/HarvestForecast.Client.Test/WorkingDaysTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using HarvestForecast.Client.Entities;
+using Xunit;
+
+namespace HarvestForecast.Client.Test;
+
+public class WorkingDaysTests
+{
+    [ Fact ]
+    public void CanActivateDayWithOperator()
+    {
+        var original = new WorkingDays( new[] {DayOfWeek.Monday, DayOfWeek.Friday} );
+
+        Assert.True( original.Monday );
+        Assert.True( original.Friday );
+        Assert.False( original.Wednesday );
+
+        var transformed = original + DayOfWeek.Wednesday;
+
+        Assert.True( transformed.Monday );
+        Assert.True( transformed.Friday );
+        Assert.True( transformed.Wednesday );
+    }
+
+    [ Fact ]
+    public void CanDeactivateDayWithOperator()
+    {
+        var original = new WorkingDays( new[] {DayOfWeek.Monday, DayOfWeek.Friday} );
+
+        Assert.True( original.Monday );
+        Assert.True( original.Friday );
+
+        var transformed = original - DayOfWeek.Monday;
+
+        Assert.False( transformed.Monday );
+        Assert.True( transformed.Friday );
+    }
+
+    [ Fact ]
+    public void CanGetDays()
+    {
+        var original = new WorkingDays( new[] {DayOfWeek.Monday, DayOfWeek.Friday} );
+
+        var days = original.GetDays().ToList();
+        Assert.Equal( 2, days.Count );
+        Assert.Contains( DayOfWeek.Friday, days );
+        Assert.Contains( DayOfWeek.Monday, days );
+    }
+}
